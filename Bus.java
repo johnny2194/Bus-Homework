707b5bcc8345e0b630d2965cac09147390b7aad4
Johnny Watson <johnny@streetchange.org.uk>
2017-05-15 19:50:59 +0100

    Add passenger count funtion

--- Bus.java
+++ Bus.java
@@ -5,5 +5,13 @@ public class Bus{
     this.passengers = new Person[6];
   }
 
-
+  public int passengerCount(){
+    int count = 0;
+    for(Person person : passengers){
+      if(person != null){
+        count++;
+      }
+    }
+    return count;
+  }
 }
--- BusTest.java
+++ BusTest.java
@@ -10,4 +10,9 @@ public class BusTest{
     bus = new Bus();
     person = new Person();
   }
+
+  @Test
+  public void busStartsEmpty(){
+    assertEquals(0, bus.passengerCount());
+  }
 }
